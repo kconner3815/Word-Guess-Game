408bf24532c03966a52a3720a04e86472d8710f0
Katie Conner <kc@Katies-MacBook-Pro.local>
2019-03-12 20:01:33 -0700

    files

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -48,39 +48,4 @@ document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
 }
 
-function checkLetters(letter) {
-
-    // This boolean will be toggled based on whether or not a user letter is found anywhere in the word.
-    var lettersInWord = false;
-  
-    // Check if a letter exists inside the array at all.
-    for (var i = 0; i < numBlanks; i++) {
-      if (chosenWord[i] === letter) {
-        // If the letter exists then toggle this boolean to true. This will be used in the next step.
-        lettersInWord = true;
-      }
-    }
-  
-    // If the letter exists somewhere in the word, then figure out exactly where (which indices).
-    if (lettersInWord) {
-  
-      // Loop through the word.
-      for (var j = 0; j < numBlanks; j++) {
-  
-        if (chosenWord[j] === letter) {
-          // Here we set the specific space in blanks and letter equal to the letter when there is a match.
-          blanksAndFilled[j] = letter;
-        }
-      }
-      // Logging for testing.
-      console.log(blanksAndFilled);
-    }
-    // If the letter doesn't exist at all...
-    else {
-      // ..then we add the letter to the list of wrong letters, and we subtract one of the guesses.
-      wrongGuesses.push(letter);
-      numOfGuesses--;
-    }
-  }
-  
-  
+function 
