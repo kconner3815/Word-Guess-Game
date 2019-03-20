5c1208b54244c5321d03ba32771bb347341a5ead
Katie Conner <kmconne2@asu.edu>
2019-03-19 23:05:29 -0700

    Update game.js

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,8 +1,17 @@
-// create a list of words for the computer to choose from by making an array 
+// create a varibale to store _ _ _ _ _ _ _ space for the keys - lettersGuessed or words guessed
+// create a var to keep track of the letters guessed 
+// create an array of the the possible words to choose from 
+// create a math. random to pick that word from the array 
+// create an alery IF the user guessess the wrong letter 
+// Create a an alert if the user guesses the word - you win! 
 
-var wordsList = ["green", "red", "yellow", "blue","white","purple"]
+// create all variables here
 
-// Declare your variables empty arrays will store future values
+
+
+var wordsList = ["green", "red", "yellow", "blue","white","purple"];
+
+// Declare variables empty arrays will store future values??
 var choosenWord = [];
 var lettersInWord = [];
 var numOfBlanks = 0;
@@ -11,7 +20,7 @@ var wrongGuesses = [];
 
 
 var wins = 0;
-var losses = 0;
+var losses = 0;  // still confused on this 1
 var guesses = 10; 
 
 // start game here 
@@ -29,12 +38,14 @@ fucntion startGame () {
 
     numOfBlanks = lettersInWord.length; 
 // empty array for restting or storing future value 
+   
     blanksAndFilled = [];
     wrongGuesses = [];
 
-// letters in picked solution
+    // letters in picked solution
   for (var i = 0; i < numOfBlanks; i++) {
-    blanksAndFilled.push("_");
+      
+      blanksAndFilled.push("_");
   }
 
 document.getElementById("guesses-left").innerHTML = numOfguesses;
@@ -43,9 +54,9 @@ document.getElementById("guesses-left").innerHTML = numOfguesses;
 document.getElementById("word-blanks").innerHTML = blanksAndFilled.join(" ");
 
 // Clears the wrong guesses from the previous round
-document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+  document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
 
 }
 
-function 
+// make a function> 
